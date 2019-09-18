f5957cb63bb8dcce1398438b446da1eb1e234ee5
saltydog79 <aspentree79@gmail.com>
2019-09-18 08:23:17 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,59 @@
 # Make your shoe class here!
+
+class Shoe 
+
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+
+    def color
+        @color
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def size
+        @size
+    end
+
+    def size=(num)
+        @size = num
+    end
+
+    def material
+        @material
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def condition
+        @condition
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+
+# How to get last test to pass?
+
+    end
+
+end
+
+
+
+
+# learn spec/02_shoe_spec.rb
